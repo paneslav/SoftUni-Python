a622ea02a0b66e4295b866eaafa60f75f46edd1c
paneslav <105970895+paneslav@users.noreply.github.com>
2022-10-27 10:38:58 +0300

    Adding stuff.

---
+++ Python_Advanced/OOP/01_first_steps_in_OOP/Exercise/project/trainer.py
@@ -0,0 +1,31 @@
+from project.pokemon import Pokemon
+
+
+class Trainer:
+    def __init__(self, name):
+        self.name = name
+        self.pokemons = []
+
+    def add_pokemon(self, pokemon: Pokemon):
+        if pokemon in self.pokemons:
+            return f"This pokemon is already caught"
+
+        self.pokemons.append(pokemon)
+        return f"Caught {pokemon.pokemon_details()}"
+
+    def release_pokemon(self, name):
+        for pokemon in self.pokemons:
+            if pokemon.name == name:
+                self.pokemons.remove(pokemon)
+                return f"You have released {name}"
+
+        return f"Pokemon is not caught"
+
+    def trainer_data(self):
+        result = f'Pokemon Trainer {self.name}\n'
+        result += f'Pokemon count {len(self.pokemons)}\n'
+
+        for pokemon in self.pokemons:
+            result += f'- {pokemon.pokemon_details()}' + '\n'
+
+        return result

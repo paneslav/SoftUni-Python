ed070f8f1a797334db6b7ef2dce268ed480ed0bd
paneslav <105970895+paneslav@users.noreply.github.com>
2024-06-12 06:30:28 +0300

    adding a solution

--- PostgreSQL/Exam_prep_1/4_11_all_volunteers_in_deps.sql
+++ PostgreSQL/Exam_prep_1/4_11_all_volunteers_in_deps.sql
@@ -11,3 +11,27 @@ $$
 	END;
 $$
 LANGUAGE plpgsql;
+
+--_______________________________________________________________________________________________________________________________________________
+
+CREATE OR REPLACE FUNCTION fn_get_volunteers_count_from_department(searched_volunteers_department VARCHAR(30))
+RETURNS INT AS
+$$
+	DECLARE
+		result INT;
+	BEGIN
+		SELECT
+			COUNT(*)
+		INTO
+			result
+		FROM
+			volunteers_departments AS vd
+		JOIN
+			volunteers AS v ON vd.id = v.department_id
+		WHERE
+			vd.department_name = searched_volunteers_department;
+
+		RETURN result;
+	END;
+$$
+LANGUAGE plpgsql;
